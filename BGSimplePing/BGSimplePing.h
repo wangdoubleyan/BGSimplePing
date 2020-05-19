eb79136629b6b4948e21ca395f10370eb05c0d68
YY <wangdoubleyan@qq.com>
2020-05-19 16:09:17 +0800

    Initial Commit

---
+++ BGSimplePing/BGSimplePing.h
@@ -0,0 +1,19 @@
+//
+//  BGSimplePing.h
+//  BGSimplePing
+//
+//  Created by YY on 2020/5/19.
+//  Copyright © 2020 sgabg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BGSimplePing.
+FOUNDATION_EXPORT double BGSimplePingVersionNumber;
+
+//! Project version string for BGSimplePing.
+FOUNDATION_EXPORT const unsigned char BGSimplePingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BGSimplePing/PublicHeader.h>
+
+
